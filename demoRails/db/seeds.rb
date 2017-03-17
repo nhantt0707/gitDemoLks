7084c282d633f00b9510a669ed89a19fc5627995
nhantt <nhant0707@gmail.com>
2017-03-17 16:03:39 +0700

    Update -user

--- demoRails/db/seeds.rb
+++ demoRails/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Example User",
+             email: "nhantt0707@gmail.com",
+             password:              "nhantt0707@gmail.com",
+             password_confirmation: "nhantt0707@gmail.com",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
