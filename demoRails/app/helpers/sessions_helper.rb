1172e0b16f38898798f4b04085fc91e67f819d69
nhantt <nhant0707@gmail.com>
2017-03-17 11:17:48 +0700

    Advanced login

--- demoRails/app/helpers/sessions_helper.rb
+++ demoRails/app/helpers/sessions_helper.rb
@@ -12,14 +12,30 @@ module SessionsHelper
       user == current_user
     end
 
-   def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-     end
+    def current_user
+     if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+    end
    def logged_in?
       !current_user.nil?
     end
 # Logs out the current user.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -27,3 +43,4 @@ module SessionsHelper
     session[:forwarding_url] = request.url if request.get?
   end
 end
+
