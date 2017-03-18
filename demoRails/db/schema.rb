9e71345c4c97a332548af85d0c3c3f05f410e9d7
nhantt <nhant0707@gmail.com>
2017-03-18 09:05:50 +0700

    Password reset

--- demoRails/db/schema.rb
+++ demoRails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317091721) do
+ActiveRecord::Schema.define(version: 20170318015741) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170317091721) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
