e99fb9a86c5106601384ee8ffa682b1483a9e96b
nhantt <nhant0707@gmail.com>
2017-03-18 11:16:38 +0700

    User follwing

--- demoRails/app/controllers/users_controller.rb
+++ demoRails/app/controllers/users_controller.rb
@@ -39,6 +39,19 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   private
   def admin_user
       redirect_to(root_url) unless current_user.admin?
